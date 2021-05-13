062c5f571347366e5a3b6f7a9c83520cc6a44927
Rhobrenz Buenavista <buenavistarhobrenz@gmail.com>
2021-05-14 07:57:37 +0800

    add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe = {
   refinedMoonsTone: 4,
 };
 
+const elvenGauntletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonsTone: 4,
+};
+
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
